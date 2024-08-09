b18cf14d05f37a221ad065cedbbec1430753c29e
NIHAT <NIHAT@DESKTOP-3F2LN5P>
2024-08-10 01:03:44 +0300

    Add project files.

---
+++ PatikaLibrary/PatikaLibrary.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaLibrary
+{
+    internal class PatikaLibrary // Class: Nesne yönelimli programlamada, veri ve işlevleri bir arada tutan yapıdır.
+    {
+
+        
+
+        private string name; // name field
+        private string authorName; // authorName field
+        private string authorSurName; // authorSurName field
+        private int pageNumber; // pageNumber field
+        private string publisher; // publisher field
+        private DateTime registration; // registration field
+
+        public string Name // Name property name field alanına güvenli ve kontrollü erişim sağlanır.
+                           // Property: Bir sınıftaki alanlara güvenli erişim sağlayan kapsayıcı özelliktir.
+        {
+            get 
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string AuthorName // AuthorName property authorName field alanına güvenli ve kontrollü erişim sağlanır.
+        {
+            get
+            {
+                return authorName;
+            }
+            set
+            {
+                authorName = value;
+            }
+        }
+
+        public string AuthorSurName // AuthorSurName property authorSurName field alanına güvenli ve kontrollü erişim sağlanır.
+        {
+            get
+            {
+                return authorSurName;
+            }
+            set
+            {
+                authorSurName = value;
+            }
+        }
+
+        public int PageNumber // PageNumber property pageNumber field alanına güvenli ve kontrollü erişim sağlanır.
+        {
+            get
+            {
+                return pageNumber;
+            }
+            set
+            {
+                pageNumber = value;
+            }
+        }
+
+        public string Publisher // Publisher property publisher field alanına güvenli ve kontrollü erişim sağlanır.
+        {
+            get
+            {
+                return publisher;
+            }
+            set
+            {
+                publisher = value;
+            }
+        }
+
+        public DateTime Registration // Registration property registration field alanına güvenli ve kontrollü erişim sağlanır.
+        { 
+            get
+            { 
+                return registration; 
+            } 
+            set 
+            {
+                registration = value;
+            }
+        }
+
+
+        public PatikaLibrary()
+        // Constructor: Bir sınıftan nesne oluşturulurken çalışan yöntemdir. Default Constructor
+        // Yayın tarihini nesne oluşturulurken veriyoruz.
+        {
+            Registration = DateTime.Now;
+        }
+
+
+        public PatikaLibrary(string name, string authorName,int pageNumber,string publisher)
+        // Constructor: Bir sınıftan nesne oluşturulurken çalışan, başlangıç değerlerini atayan özel yöntemdir. Parametreli Constructor
+        // Kitap adı, Yazar Adı, Sayfa Sayısı, Yayınevi  bilgilerini alıp bu değerleri nesne üretim aşamasında atayan bir metot
+        // Yayın tarihini nesne oluşturulurken veriyoruz.
+
+        {
+            Name = name;
+            AuthorName = authorName;            
+            PageNumber = pageNumber;
+            Publisher = publisher;
+            Registration = DateTime.Now;
+            
+
+        }
+
+        public void TellInfo()
+        {
+            Console.WriteLine($" ile oluşturulan kitap nesnesi\nKitap Adı : {name}\nYazar Adı : {authorName} \nSayfa Sayısı : {pageNumber}\nYayınevi : {publisher}\nKayıt Tarihi : {registration.ToString("dd.MM.yyyy")}");
+        }
+
+
+
+    }
+}
+
---
+++ PatikaLibrary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace PatikaLibrary
+{
+    class Program // Class: Nesne yönelimli programlamada, veri ve işlevleri bir arada tutan yapıdır.
+    {
+        public static void Main(string[] args)
+        {
+            // New: Sınıftan yeni bir nesne oluşturmak için kullanılan anahtar kelimedir.
+            PatikaLibrary newBook1 = new PatikaLibrary(); // Parametre almadan oluşturulan nesne            
+            newBook1.Name = "Adı Aylin";
+            newBook1.AuthorName = "Ayşe";
+            newBook1.AuthorSurName = "Kulin";
+            newBook1.PageNumber = 398;
+            newBook1.Publisher = "Remzi Kitabevi";            
+            Console.Write("Default Constructor");
+            newBook1.TellInfo();
+
+            // New: Sınıftan yeni bir nesne oluşturmak için kullanılan anahtar kelimedir.
+            // Kitap adı, Yazar Adı, Sayfa Sayısı, Yayınevi  bilgilerini alıp bu değerleri nesne üretim aşamasında atayan bir metot
+            PatikaLibrary newBook2 = new PatikaLibrary("Daha Başka Kitap","Mehmet",450,"Gamma Yayınları"); // Parametre alarak oluşturulan nesne
+            Console.Write("\nParametreli Constructor");
+            newBook2.TellInfo();
+        }
+    }
+}
